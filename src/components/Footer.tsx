51c71df353c6348340e9a4ec66814c7f3820dbfd
sheoranruns <itspriyanshu1308@gmail.com>
2026-04-19 00:51:40 +0530

    Final Code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://github.com/sheoranruns" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/priyanshu-sheoran/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
       <p>A portfolio built by <a href="https://github.com/sheoranruns/portfolio" target="_blank" rel="noreferrer">Priyanshu Sheoran</a> with 💜</p>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,14 +13,14 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/sheoranruns" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/priyanshu-sheoran/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Priyanshu Sheoran</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/sheoranruns" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/priyanshu-sheoran/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
